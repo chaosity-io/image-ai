64c1989d559a81a232d38d5ef8f802f2a77cf6ea
Mehdi Jorshari <mjorshari@jaycar.com.au>
2024-10-28 15:34:23 +1100

    morechange

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -14,21 +14,25 @@ if (!cached) {
 }
 
 export const connectToDatabase = async () => {
-    if (cached.conn) {
+    try {
+
+        if (cached.conn) {
+            return cached.conn;
+        }
+        console.log('Connecting to MongoDB...');
+        if (!MONGODB_URL) throw new Error("Please define a MongoDB connection");
+        console.log('MongoDB URL:', MONGODB_URL);
+        const opts = {
+            dbName: 'imaginify',
+            bufferCommands: false,
+        }
+        cached.promise = cached.promise || mongoose.connect(MONGODB_URL!, opts);
+        console.log('Promise:', cached.promise);
+        cached.conn = await cached.promise;
+        console.log('Connected:', cached.conn);
+        console.log('Connected to MongoDB');
         return cached.conn;
+    } catch (error) {
+        console.log(error);
     }
-
-    if (!MONGODB_URL) throw new Error("Please define a MongoDB connection");
-
-    const opts = {
-        dbName: 'imaginify',
-        bufferCommands: false,
-    }
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URL!, opts);
-
-    cached.conn = await cached.promise;
-
-    console.log('Connected to MongoDB');
-    return cached.conn;
-
 }
